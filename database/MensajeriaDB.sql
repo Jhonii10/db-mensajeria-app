0172d5d4a0bc546d285977fed31533edc00c114d
Vtinasr <valentina.sanchez.rosero@correounivalle.edu.co>
2024-05-28 13:06:20 -0500

    DB Correcciones

--- database/MensajeriaDB.sql
+++ database/MensajeriaDB.sql
@@ -1,24 +1,24 @@
 CREATE TABLE users (
-    ID_User SERIAL PRIMARY KEY,
-    Name VARCHAR(100) NOT NULL,
-    Email VARCHAR(100) NOT NULL UNIQUE,
-    Login VARCHAR(50) NOT NULL UNIQUE,
-    Password VARCHAR(255) NOT NULL,
-    Rol VARCHAR(50) NOT NULL,
-    Address VARCHAR(255),
-    Cell_phone INT,
-    CHECK (Rol IN ('Customer', 'Delivery', 'Manager'))
+  ID_User SERIAL PRIMARY KEY,
+  Name VARCHAR(100) NOT NULL,
+  Email VARCHAR(100) NOT NULL UNIQUE,
+  Login VARCHAR(50) NOT NULL UNIQUE,
+  Password VARCHAR(255) NOT NULL,
+  Role VARCHAR(50) NOT NULL,
+  Address VARCHAR(255),
+  Cell_phone BIGINT, 
+  CHECK (Role IN ('Customer', 'Delivery', 'Manager'))
 );
 
 CREATE TABLE customer (
     ID_Customer INT PRIMARY KEY,
     ID_User INT UNIQUE,
     City VARCHAR(100),
-    FOREIGN KEY (ID_users) REFERENCES users (ID_users) ON DELETE CASCADE
+    FOREIGN KEY (ID_User) REFERENCES users (ID_User) ON DELETE CASCADE
 );
 
 CREATE TABLE delivery (
---ID_Number es el cc del mensajero 
+    --ID_Number is the identification number of the delivery person 
     ID_Number INT PRIMARY KEY, 
     ID_User INT NOT NULL,
     ID_Delivery VARCHAR(50) NOT NULL,
@@ -29,7 +29,7 @@ CREATE TABLE branch (
     ID_Branch SERIAL PRIMARY KEY,
     Name_Store VARCHAR(100) NOT NULL,
     Address VARCHAR(255),
-    Cell_phone INT,
+    Cell_phone BIGINT,
     ID_Customer INT,
     FOREIGN KEY (ID_Customer) REFERENCES customer (ID_Customer) ON DELETE CASCADE
 );
@@ -37,7 +37,7 @@ CREATE TABLE branch (
 CREATE TABLE service (
     ID_Service SERIAL PRIMARY KEY,
     Service_Code SERIAL UNIQUE,
-    Date DATE,
+    Service_Date DATE,
     Origin VARCHAR(255),
     Destination VARCHAR(255),
     Number_of_Packages INT,
@@ -46,14 +46,14 @@ CREATE TABLE service (
     City VARCHAR(100),
     CHECK (Type_of_Transport IN ('Car', 'Motorcycle', 'Truck')),
     ID_Customer INT, 
-    ID_Delivery VARCHAR (50)	
+    ID_Delivery VARCHAR (50)    
 );
 
 CREATE TABLE status (
     ID_Status SERIAL PRIMARY KEY,
-    Date DATE,
+    Status_Date DATE,
     Type_Status VARCHAR(20),
-    CHECK(Type_Status IN ('Delivery','Picked Up','Required')), 
+    CHECK(Type_Status IN ('Delivery', 'Picked Up', 'Required')), 
     ID_Service INT UNIQUE,
     FOREIGN KEY (ID_Service) REFERENCES service (ID_Service) ON DELETE CASCADE
 );
@@ -61,8 +61,7 @@ CREATE TABLE status (
 CREATE TABLE photo (
     ID_Photo SERIAL PRIMARY KEY,
     URL_Images VARCHAR(255),
-    Date DATE,
+    Photo_Date DATE,
     ID_Service INT,
     FOREIGN KEY (ID_Service) REFERENCES service (ID_Service) ON DELETE CASCADE
 );
-
